17553d41d6112a1495f4203bc09cc2453a693e60
manny <jhonnyherrera1998@gmail.com>
2023-05-06 01:02:31 -0500

    registrarse

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
+import { RegistrarseComponent } from './component/registrarse/registrarse.component';
 
 const routes: Routes = [
-  {path:'login', component:LoginComponent}
+  {path:'login', component:LoginComponent},
+  {path:'registrarse', component:RegistrarseComponent}
 ];
 
 @NgModule({
