00206abdabefe4c751557dc047e2fe935f747ae1
Steven Bazan <ste_alex_bazan@hotmail.com>
2023-06-19 20:39:22 -0500

    Barra de navegacion admin

--- ClientApp/src/app/component/admin/admin/admin.component.ts
+++ ClientApp/src/app/component/admin/admin/admin.component.ts
@@ -10,6 +10,26 @@ export class AdminComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const submenuItems = document.querySelectorAll('.submenu');
+
+    submenuItems.forEach((submenu) => {
+      const parentMenuItem = submenu.parentElement;
+      const menuItemLink = parentMenuItem?.querySelector('.alink');
+
+      if (parentMenuItem && menuItemLink) {
+        menuItemLink.addEventListener('click', (event) => {
+          event.preventDefault();
+
+          const submenuElement = submenu as HTMLElement;
+          if (submenuElement.style.display === 'block') {
+            submenuElement.style.display = 'none';
+          } else {
+            submenuElement.style.display = 'block';
+          }
+        });
+      }
+    });
   }
 
 }
+
